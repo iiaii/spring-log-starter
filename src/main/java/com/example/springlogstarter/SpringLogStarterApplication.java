c5cfdb4949cee1dd18ff007b8114d209d9e28da0
iiaii <jhun9409@naver.com>
2022-04-04 23:50:42 +0900

    feat: v0 project setting

---
+++ src/main/java/com/example/springlogstarter/SpringLogStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.springlogstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLogStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLogStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springlogstarter/SpringLogStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springlogstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLogStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
